269caab821fcae472c9ef213c65393140a500827
TsMask <340112800@qq.com>
2023-06-11 21:57:56 +0800

    fix: 补充文件操作异常信息输出

--- src/framework/utils/FileUtils.ts
+++ src/framework/utils/FileUtils.ts
@@ -22,9 +22,10 @@ export async function getFileSize(filePath: string): Promise<number> {
   try {
     const { size } = await stat(filePath);
     return size || 0;
-  } catch (_) {
-    return 0;
+  } catch (error) {
+    console.error(error);
   }
+  return 0;
 }
 
 /**
@@ -37,7 +38,8 @@ export async function checkExists(filePath: string): Promise<boolean> {
   try {
     await access(filePath, constants.F_OK);
     return true;
-  } catch (_) {
+  } catch (error) {
+    console.error(error);
     return false;
   }
 }
@@ -50,7 +52,8 @@ export async function checkDirPathExists(dirPath: string) {
   if (!dirPath) return;
   try {
     await access(dirPath, constants.F_OK);
-  } catch (_) {
+  } catch (error) {
+    console.error(error);
     await mkdir(dirPath, { recursive: true });
   }
 }
@@ -72,7 +75,9 @@ export async function deleteFile(absPath: string): Promise<boolean> {
       await rmdir(absPath);
       return true;
     }
-  } catch (_) {}
+  } catch (error) {
+    console.error(error);
+  }
   return false;
 }
 
@@ -115,7 +120,7 @@ export async function getFileStream(
     await fd.read(buffer, 0, chunkSize, start);
     return buffer;
   } catch (error) {
-    throw error;
+    console.error(error);
   } finally {
     await fd.close();
   }
@@ -154,7 +159,9 @@ export async function getDirFileNameList(dirPath: string): Promise<string[]> {
       }
     }
     return fileNames;
-  } catch (_) {}
+  } catch (error) {
+    console.error(error);
+  }
   return [];
 }
 
@@ -165,7 +172,7 @@ export async function getDirFileNameList(dirPath: string): Promise<string[]> {
  * @return 文件类型后缀（含“.”）
  */
 export function getBufferFileExtendName(buf: Buffer): string {
-  const header = buf.slice(0, 10);
+  const header = buf.subarray(0, 10);
   const headers = [
     { header: [71, 73, 70, 56, 55, 97], ext: '.gif' },
     { header: [71, 73, 70, 56, 57, 97], ext: '.gif' },
@@ -200,8 +207,8 @@ export async function writeBufferFile(
   // 写入到新路径文件
   try {
     await writeFile(filePath, buf);
-  } catch (err) {
-    throw new Error('文件写入失败' + err.message);
+  } catch (error) {
+    console.error(error);
   }
   return filePath;
 }
